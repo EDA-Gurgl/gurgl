36c45038c72d2dfb2a56fa80618a8c6246e55aff
Thomasin Abraham <thomasinabraham@me.com>
2017-06-14 13:18:48 +1200

    Hopefully this boilerplate working

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Greetings from './Greetings'
+
+const App = () => (
+  <div className='app-container'>
+    <Greetings />
+  </div>
+)
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,25 @@
+var path = require('path')
+var express = require('express')
+var bodyParser = require('body-parser')
+const cors = require('cors')
+
+
+var greetings = require('./routes/greeting')
+
+
+const corsOptions = {
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  credentials: true
+}
+
+var server = express()
+server.use(cors(corsOptions))
+
+server.use(bodyParser.json())
+server.use(express.static(path.join(__dirname, '../public')))
+
+server.use('/api/greetings', greetings)
+
+module.exports = server
