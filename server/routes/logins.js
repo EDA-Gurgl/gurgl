75a754cd7971c37b65467d59bde2fda45d5d9b11
Alisa Yunusova <alisa.yunusova@gmail.com>
2017-06-15 12:06:17 +1200

    created first protected route

--- server/routes/logins.js
+++ server/routes/logins.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 var create = require('../db/members').create
 var auth = require('../lib/auth')
-
+var verifyJwt = require('express-jwt')
 
 router.post('/register', (req, res, next) => {
   create(req.body.username, req.body.password, req.app.get('db'))
@@ -12,5 +12,22 @@ router.post('/register', (req, res, next) => {
 
 
 router.post('/login', auth.issueJwt)
+
+
+///routes below are private
+router.use(
+  verifyJwt({
+    secret: getSecret
+  }),
+  auth.handleError
+)
+
+router.get('/account', (req, res) => {
+  res.send('hi')
+})
+
+function getSecret (req, payload, done) {
+  done(null, process.env.JWT_SECRET)
+}
 
 module.exports = router
