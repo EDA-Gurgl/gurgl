d7b578e8ef3e98e7a6c0f47d81033627c808ca69
Bev Walter <bevwaltersemail@gmail.com>
2017-06-15 18:23:26 +1200

    Added joins and another detail to FAQ

--- server/db/clothing.js
+++ server/db/clothing.js
@@ -3,16 +3,33 @@ const getClothing = (db) => {
     .select(
       '*',
       'size.description as size_description',
-      'brand.description as brand_description')
+      'brand.description as brand_description',
+      'style.description as style_description',
+      'condition.description as condition_description',
+      'status.description as status_description')
+
     .join('size', 'clothing.size_id', '=', 'size.id')
     .join('brand', 'clothing.brand_id', '=', 'brand.id')
     .join('style', 'clothing.style_id', '=', 'style.id')
+    .join('condition', 'clothing.condition_id', '=', 'condition.id')
+    .join('status', 'clothing.status_id', '=', 'status.id')
 }
 
 const getSingleItem = (db, id) => {
   return db('clothing')
-    .select('*')
-    .where('id', id)
+     .select(
+       '*',
+       'size.description as size_description',
+       'brand.description as brand_description',
+       'style.description as style_description',
+       'condition.description as condition_description',
+       'status.description as status_description')
+
+    .join('size', 'clothing.size_id', '=', 'size.id')
+    .join('brand', 'clothing.brand_id', '=', 'brand.id')
+    .join('style', 'clothing.style_id', '=', 'style.id')
+    .join('condition', 'clothing.condition_id', '=', 'condition.id')
+    .join('status', 'clothing.status_id', '=', 'status.id')
 }
 
 module.exports = {
