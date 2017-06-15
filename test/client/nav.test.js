5e4bf2c4d1dc1eb5658be40649d75c867f727f7c
Thomasin Abraham <thomasinabraham@me.com>
2017-06-16 09:11:00 +1200

    10 tests passing

--- test/client/nav.test.js
+++ test/client/nav.test.js
@@ -20,6 +20,16 @@ test('Clicking on search submit button fires off setSearch action', t => {
   t.is(store.dispatch.calledWith({ type:'RECEIVE_SEARCH', searchTerm: 'test'}), true)
  })
 
+test('Clicking on search submit button when not on clothing page redirects', t => {
+  let historyFunc = sinon.stub()
+  const wrapper = mount(<Nav store={store} location={{pathname: '/'}} history={{push: historyFunc}}/>)
+  wrapper.find('input[name="searchBar"]')
+    .simulate('change', {target: {name: 'searchBar', value: 'test'}})
+  wrapper.find('button[name="searchSubmit"]')
+    .simulate('click')
+  t.is(historyFunc.calledWith('/clothing'), true)
+ })
+
 test('Nav has a search bar', t => {
   const wrapper = mount(<Nav store={store} location={{pathname: '/clothing'}}/>)
   t.is(wrapper.find('input[name="searchBar"]').exists(), true)
