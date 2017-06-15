2de3732b97e689fc866e419186ff6774e02c2196
Andrew <andy@andycondon.com>
2017-06-15 20:11:37 +1200

    testing bah!

--- test/client/actions.test.js
+++ test/client/actions.test.js
@@ -6,4 +6,5 @@ test('Setting search returns correct object', t => {
   let searchObject = setSearch('testing')
   t.is(searchObject.type, 'RECEIVE_SEARCH')
   t.is(searchObject.searchTerm, 'testing')
+  
 })
