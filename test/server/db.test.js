9b6cd06c11b1ba1de202b59df6b7117d3eb2871d
Andrew <andy@andycondon.com>
2017-06-16 14:45:39 +1200

    passing 17 tests

--- test/server/db.test.js
+++ test/server/db.test.js
@@ -8,7 +8,7 @@ var db = require('../../server/db/clothing')
 test('getClothing returns all the kit', t => {
   return db.getClothing(t.context.connection)
     .then((result) => {
-      t.is(result[0].id, 51)
+      t.is(result[0].id, 100)
       t.not(result.length, 0)
     })
 })
@@ -16,8 +16,6 @@ test('getClothing returns all the kit', t => {
 test('getSingleItem returns the onesie we want', t => {
   return db.getSingleItem(t.context.connection, 121)
     .then((result) => {
-      console.log(result);
-      // t.is(result.length, 1)
       t.is(result.id, 121)
       t.is(result.size_description, '6-9 months')
       t.is(result.photo1, '/images/babyg_3.jpg')
