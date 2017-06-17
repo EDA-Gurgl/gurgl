b92ce2dbdeee2b54c9a590e4933e08857e111221
Bev Walter <bevwaltersemail@gmail.com>
2017-06-17 17:05:26 +1200

    Removed bundle

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 const Landing = () =>
    (
    <div className="Landing">
-      I am the landing or home page!
+      <h2> Welcome to Gurgl</h2>
+      <h3> Your one stop shop for the cutest pre-loved infant clothing </h3>
+      <p>
+      Gurgl's mission is to save you money. Save resources and hopefully save our plant!
+      </p>
+      <p>
+      Through donations from individuals - like you, and organisations with clothing to spare. We plan on creating a circular-economy. Where we reduce the amount of newly purchased fast-fashion items in production. Replacing them with ecocycled rescued items with lots more love to give.
+      </p>
+      <p>
+      We provide a fun, easy platform for you to recycle your childrens wardrobes, and borrow more! With your help we can redirect perfectly good clothing out of the landfill waste pile and onto the backs of children who need them
+      </p>
+      <p>
+      We can play our part. Have a positive impact on our plant and leave it in a better pre-loved condition for the next generations of Kiwis.
+      </p>
    </div>
    )
 
--- seeds/clothingDetails.js
+++ seeds/clothingDetails.js
@@ -18,7 +18,7 @@ exports.seed = function (knex, Promise) {
         {
           id: 120,
           size_id: 35,
-          style_id: 4,
+          style_id: 8,
           brand_id: 2,
           condition_id: 21,
           status_id: 51,
@@ -29,18 +29,18 @@ exports.seed = function (knex, Promise) {
         {
           id: 121,
           size_id: 34,
-          style_id: 3,
+          style_id: 8,
           brand_id: 1,
           condition_id: 24,
           status_id: 51,
-          description: 'As aborable! The best choice for all you gym mums out there in pretty good condition.  Please note there is a tiny stain at the neck. Seems longer than other onesie suits. Be a great summer option.',
+          description: 'So adorable! The best choice for all you gym mums out there in pretty good condition.  Please note there is a tiny stain at the neck. Seems longer than other onesie suits. Be a great summer option.',
           photo1: '/images/babyg_3.jpg',
           photo2: ''
         },
         {
           id: 122,
           size_id: 37,
-          style_id: 6,
+          style_id: 8,
           brand_id: 5,
           condition_id: 22,
           status_id: 51,
@@ -51,9 +51,9 @@ exports.seed = function (knex, Promise) {
         {
           id: 123,
           size_id: 35,
-          style_id: 7,
+          style_id: 8,
           brand_id: 3,
-          condition_id: 21,
+          condition_id: 22,
           status_id: 51,
           description: 'So funny! What a talking point -  in nice condition. Has small dye mark on cuffs. Fit comes up larger than expected. Would look super cute with jeans and a hoody for winter.',
           photo1: 'http://cdn.shopify.com/s/files/1/1140/3192/products/2012_baby_babygrow_just-done-9-months-inside_lack-_-white_1_grande.png?v=1458568176',
@@ -73,7 +73,7 @@ exports.seed = function (knex, Promise) {
       {
         id: 125,
         size_id: 35,
-        style_id: 4,
+        style_id: 8,
         brand_id: 2,
         condition_id: 21,
         status_id: 51,
@@ -84,9 +84,9 @@ exports.seed = function (knex, Promise) {
       {
         id: 126,
         size_id: 37,
-        style_id: 7,
+        style_id: 3,
         brand_id: 5,
-        condition_id: 22,
+        condition_id: 23,
         status_id: 51,
         description: 'Very grown up little dress, in really nice condition. Please note there is slight pilling on rear. Fit comes up slightly small. Suitable as a warm layer or for use in colder weather.',
         photo1: '/images/dress_1.jpg',
@@ -95,7 +95,7 @@ exports.seed = function (knex, Promise) {
       {
         id: 127,
         size_id: 35,
-        style_id: 7,
+        style_id: 3,
         brand_id: 3,
         condition_id: 21,
         status_id: 51,
@@ -106,7 +106,7 @@ exports.seed = function (knex, Promise) {
       {
         id: 128,
         size_id: 37,
-        style_id: 7,
+        style_id: 3,
         brand_id: 5,
         condition_id: 22,
         status_id: 51,
@@ -116,15 +116,63 @@ exports.seed = function (knex, Promise) {
       },
       {
         id: 129,
-        size_id: 35,
-        style_id: 7,
+        size_id: 32,
+        style_id: 3,
         brand_id: 3,
         condition_id: 21,
         status_id: 51,
         description: 'Great for warm summer afternoons, or layered with leggings and a long sleeve t-shirt for when its cooler. Please note the elastic on the waste is a little worn, so it fits loosely, great for littlies who do not like being too wrapped up.',
         photo1: '/images/dress_4.jpg',
         photo2: ''
-      }
+      },
+      {
+        id: 130,
+        size_id: 30,
+        style_id: 8,
+        brand_id: 3,
+        condition_id: 21,
+        status_id: 51,
+        title:'Rainbows for the win',
+        description: 'Long sleeved suit, that seems narrow. Lovely thick jersey, so your little rainbow can be as warm as toast! Room to grow if your bubba is going to be tall. Really impressed by the condition of this item.',
+        photo1: 'https://littlepeagreen.files.wordpress.com/2012/04/frugi_rainbow_spot-babygrow-2.png',
+        photo2: ''
+     },
+     {
+       id: 131,
+       size_id: 30,
+       style_id: 8,
+       brand_id: 3,
+       condition_id: 21,
+       status_id: 51,
+       title:'Ute will love this!',
+       description: 'Little blue VW combie on the front. Hand sewn by a boutique supplier. Very cute, you can just tell the love that goes into creating these little NZ made vests. A great addition to you little ones wardrobe.',
+       photo1: 'https://cdn.notonthehighstreet.com/system/product_images/images/000/491/279/original_Blue_campervan_vest.jpg',
+       photo2: ''
+    },
+    {
+      id: 132,
+      size_id: 34,
+      style_id: 8,
+      brand_id: 3,
+      condition_id: 21,
+      status_id: 51,
+      title:'Pink geek',
+      description: 'Oh my goodness so pink, honestly the most adorable thing I have seen in ages. It does have a little hole in the back but otherwise in great condition. If your little one knows how to make a statement then this is the outfit for you!',
+      photo1: 'https://s3-eu-west-1.amazonaws.com/images.linnlive.com/4a3fc06eb1b04726bf024759db185f2f/b82257af-c06c-4669-bd60-949786611550.jpg',
+      photo2: ''
+      },
+      {
+      id: 133,
+      size_id: 37,
+      style_id: 8,
+      brand_id: 3,
+      condition_id: 21,
+      status_id: 51,
+      title:'Maths Nerd',
+      description: 'I do not think you have to be a nerd to love this onesie. Or to get the joke... This top is a lovely soft shade of blue and feels like brushed jersey cotton. The paint has chipped off the poppers, which is not noticeable when pants or skirt are added.',
+      photo1: 'https://images-na.ssl-images-amazon.com/images/I/51OKFQZff6L._AC_UL320_SR244,320_.jpg',
+      photo2: ''
+      },
       ])
     })
 }
