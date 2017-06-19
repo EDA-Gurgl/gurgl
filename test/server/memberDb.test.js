584f4a9c61ebda326f7d1a8ef4deb7f5153a4098
Thomasin Abraham <thomasinabraham@me.com>
2017-06-19 13:37:31 +1200

    member db tests passing

--- test/server/memberDb.test.js
+++ test/server/memberDb.test.js
@@ -14,9 +14,42 @@ test('getById returns correct user', t => {
 })
 
 test('getByName returns correct user', t => {
-  return db.getById(51, t.context.connection)
+  return db.getByName('bev', t.context.connection)
     .then((result) => {
       t.is(result[0].id, 51)
       t.is(result.length, 1)
+    })
+})
+
+test('create succesfully creates new user', t => {
+  let user = {
+    name: 'Test user',
+    username: 'testy test test',
+    password: 'testing',
+    address: '10 Test Street',
+    phone: '0215887843',
+    email: 'test@testing.com'
+  }
+  return db.create(user, t.context.connection)
+    .then((result) => {
+      return db.getByName('testy test test', t.context.connection)
+    })
+    .then((result) => {
+      t.is(result[0].name, 'Test user')
+      t.is(result[0].address, '10 Test Street')
+    })
+})
+
+test("exists returns false if user doesn't exist", t => {
+  return db.exists('im not exist', t.context.connection)
+    .then((result) => {
+      t.is(result, false)
+    })
+})
+
+test("exists returns true if user does exist", t => {
+  return db.exists('bev', t.context.connection)
+    .then((result) => {
+      t.is(result, true)
     })
 })
