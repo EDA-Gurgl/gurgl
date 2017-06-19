c79a33fccf32a455d3622bf64b9805d506bb8e24
Alisa Yunusova <alisa.yunusova@gmail.com>
2017-06-19 16:33:32 +1200

    add /login api route test

--- test/server/api.test.js
+++ test/server/api.test.js
@@ -1,7 +1,9 @@
 import test from 'ava'
 import request from 'supertest'
+const decode = require('jwt-decode')
 
 import server from '../../server/server'
+
 
 let configureDatabase = require('./helpers/database-config')
 configureDatabase(test, server)
@@ -49,5 +51,32 @@ test.cb('POST /register ', t => {
       t.is(members.length, originalCount+1)
       t.end()
     })
+  })
+})
+
+
+test.cb.only('POST /login ', t => {
+  process.env.JWT_SECRET='secret'
+  const existingUser = {
+    username: 'existinguser',
+    password: 'password'
+  }
+
+  request(t.context.server)
+    .post('/api/v1/login')
+    .send(existingUser)
+    .expect(200)
+    .end((err, res) => {
+      t.is(decode(res.body.token).name, 'Existing User')
+      request(t.context.server)
+      ///test private route
+      .get('/api/v1/account')
+      .set('Authorization', `Bearer ${res.body.token}`)
+      .expect(200)
+      .end((err, res) => {
+              if (err) throw err
+              t.is(res.body.user, 'Your user ID is: 61')
+              t.end()
+      })
   })
 })
